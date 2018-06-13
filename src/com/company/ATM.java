f147b793bd0ae74ff27111128da578e5de000ece
zele <zelalem.agmasse@gmail.com>
2018-06-12 20:15:06 -0400

    final version

--- src/com/company/ATM.java
+++ src/com/company/ATM.java
@@ -5,9 +5,9 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class ATM {
-    private Integer accntHolderNum;
+    private String accntHolderNum;
     private String accntPin;
-    private String options;
+    private int options;
     private double custMoney;
     Scanner input= new Scanner(System.in);
     Account userA= new Account(1632,"James",124000,"palmTree");
@@ -24,39 +24,52 @@ public class ATM {
     }
     public void customerService(){
         System.out.println("WELCOME TO XYZ BANK");
-        System.out.println("Please Enter the Accoount and Password");
-        accntHolderNum=input.nextInt();
-        System.out.println("and PASSWORD");
-        accntPin=input.nextLine();
-        if(accountVerifier(accntHolderNum,accntPin)){
+        int counter=0;
+      // try {
+           while (!(counter == 3)) {
+               System.out.println("Please Enter the Accoount and Password");
+               accntHolderNum = input.nextLine();
+               int intValue = Integer.parseInt(accntHolderNum);
+               System.out.println("and PASSWORD");
+               accntPin = input.nextLine();
+
+               if (accountVerifier(intValue, accntPin)) {
 
 
-            Account workingAcount=userAccount.get(accntHolderNum);
+                   Account workingAcount = userAccount.get(intValue);
 
 
-                workingAcount.accountInfoDisplay();
-                System.out.println("what do you want to do with your account ?");
-                System.out.println("please select from the below choices");
-                System.out.println("(1)-WITHDRAW  , (2)- DEPOSIT");
-                options= input.nextLine();
-                System.out.println(opsSelector(options));
-                custMoney=input.nextDouble();
-                System.out.println(" Your new Balance will be " + accoutTransaction(options,custMoney,workingAcount));
+                   workingAcount.accountInfoDisplay();
+                   System.out.println("what do you want to do with your account ?");
+                   System.out.println("please select from the below choices");
+                   System.out.println("(1)-WITHDRAW  , (2)- DEPOSIT");
+                   options = input.nextInt();
+                   System.out.println(opsSelector(options));
+                   custMoney = input.nextDouble();
+                   System.out.println(" Your new Balance will be " + accoutTransaction(options, custMoney, workingAcount));
+                   break;
 
-        }
+               }
+               counter++;
+           }
+           if (counter==3)
+               System.out.println("SORRY YOU ACCOUNT HAS BEEN BLOCKED ! CONTACT THE BANK ADMINISTRATORS");
+      // }catch(Exception e){
+        //
+     //  }
 
     }
 
-public  double accoutTransaction(String choice,double trans,Account acct){
-    switch (choice.toLowerCase())
+public  double accoutTransaction(int choice,double trans,Account acct){
+    switch (choice)
     {
 
-        case"withdrawal" : double balanceWith=acct.getBalance()-trans;
+        case 1 : double balanceWith=acct.getBalance()-trans;
             acct.setBalance(balanceWith);
 
 
             break;
-        case"deposit": double balanceDep=acct.getBalance()+trans;
+        case 2: double balanceDep=acct.getBalance()+trans;
             acct.setBalance(balanceDep);
 
 
@@ -86,13 +99,15 @@ public double accoutDeposit(double depo,Account acct){
     public Account accountOpener(String acctName){
        return userAccount.get(acctName);
     }
-    public String opsSelector(String choice){
+    public String opsSelector(int choice){
         String displayMessge=new String();
-        switch (choice.toLowerCase()){
+        switch (choice){
 
-            case"withdrawal" : displayMessge="PLEASE Enter the amount of money to withdraw";
+            case 1 : displayMessge="PLEASE Enter the amount of money to withdraw";
             break;
-            case"deposit": displayMessge="Please Enter the amount money youw ant to deposit in your account";
+            case 2: displayMessge="Please Enter the amount money youw ant to deposit in your account";
+            break;
+
 
         }
         return displayMessge;
